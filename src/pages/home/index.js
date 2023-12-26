01a2599f680518233cf804332de18ea0c7a2306c
Bruno A. S. Guimarães <71555944+nadapracoloca@users.noreply.github.com>
2023-12-26 09:44:46 -0300

    Add files via upload

---
+++ src/pages/home/index.js
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Modal,
+} from "react-native";
+import Slyder from "@react-native-community/slider";
+import { ModalPassword } from "../../components/modal";
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+export function Home() {
+  const [size, setSize] = useState(10);
+  const [passwordValue, setPasswordValue] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
+
+  function generatePassword() {
+    let password = "";
+    for (let i = 0, n = charset.length; i < size; i++) {
+      password += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    setPasswordValue(password);
+    setModalVisible(true);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image source={require("../../assets/logo.png")} style={styles.logo} />
+
+      <Text style={styles.title}>{size} Caracteres</Text>
+
+      <View style={styles.area}>
+        <Slyder
+          style={{ height: 50 }}
+          minimumValue={1}
+          maximumValue={25}
+          maximumTrackTintColor="#ff0000"
+          thumbTintColor="#392"
+          value={size}
+          onValueChange={(value) => setSize(value.toFixed(0))}
+        />
+      </View>
+
+      <TouchableOpacity style={styles.button} onPress={generatePassword}>
+        <Text style={styles.buttonText}>Gerar Senha</Text>
+      </TouchableOpacity>
+
+      <Modal visible={modalVisible} animationType="fade" transparent={true}>
+        <ModalPassword
+          password={passwordValue}
+          handleClose={() => setModalVisible(false)}
+        />
+      </Modal>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F3F3FF",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    marginBottom: 60,
+  },
+  area: {
+    marginTop: 14,
+    marginBottom: 14,
+    width: "80%",
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    padding: 2,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  button: {
+    backgroundColor: "#392de9",
+    width: "80%",
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+    marginBottom: 18,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 20,
+  },
+});
